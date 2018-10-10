136d49cc06a16bd08b6ea4b527c48b037a70acd8
iceteatwo <mikkelpedersen92@gmail.com>
2018-10-10 11:03:11 +0200

    migrating to windows

---
+++ RAWServer/Models/RDJTPMethod.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RAWServer.Models
+{
+    public enum RDJTPMethod
+    {
+        ok,
+        error,
+        not_found
+    }
+}
--- RAWServer/Models/RDJTPRequest.cs
+++ RAWServer/Models/RDJTPRequest.cs
@@ -9,7 +9,8 @@ namespace RAWServer.Models
 
 
 
-        public string Method { get; set; }
+
+        public RDJTPMethod Method { get; set; }
         public string Path { get; set; }
         public string Date { get; set; }
         public string Body { get; set; }
